af02336f2bd6d1877949320d75f2da4d6a3cc010
Andrew Smith <andrew.smith5569@gmail.com>
2018-12-27 12:53:29 -0500

    refactor - renamed and moved files into directories

---
+++ src/component/Hello.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Hello.css";
+
+export class Hello extends Component {
+  render() {
+    return <div className="hello">Hello, World!</div>;
+  }
+}
