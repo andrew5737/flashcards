a57d7bfc712f994cf88d8d987b31137bc37f0453
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-01 08:50:12 -0500

    removed: global.console.error override in setupTest.js no longer needed since the test suite no longer uses jsdom

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,3 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
-
-// override jsdom console.error so that it does not log errors during tests
-// errors will be handled normally
-global.console.error = () => {};
