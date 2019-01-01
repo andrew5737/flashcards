356e618bfd388492b68164689a353c3efff5e131
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-01 11:01:18 -0500

    added: onDeckSelected prop to Deck

--- src/component/Deck/Deck.spec.js
+++ src/component/Deck/Deck.spec.js
@@ -18,4 +18,20 @@ describe("<Deck />", () => {
     const wrapper = shallow(<Deck name={name} />);
     expect(wrapper.find(".deck--name").text()).toBe(name);
   });
+
+  it("calls props.onDeckSelected when deck is clicked", () => {
+    const onDeckSelectedMock = jest.fn();
+    const wrapper = shallow(<Deck name={"name"} onDeckSelected={onDeckSelectedMock} />);
+    wrapper.find(".deck").simulate("click");
+    expect(onDeckSelectedMock.mock.calls.length).toBe(1);
+  });
+
+  it("passes deck.id to props.onDeckSelected when deck is clicked", () => {
+    const onDeckSelectedMock = jest.fn();
+    const deckId = 1214;
+    const wrapper = shallow(<Deck name={"name"} id={deckId} onDeckSelected={onDeckSelectedMock} />);
+    wrapper.find(".deck").simulate("click");
+    expect(onDeckSelectedMock.mock.calls.length).toBe(1);
+    expect(onDeckSelectedMock.mock.calls[0][0]).toBe(deckId);
+  });
 });
--- src/component/Deck/index.js
+++ src/component/Deck/index.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import "./Deck.css";
 
 export class Deck extends Component {
+  onDeckSelected = () => {
+    this.props.onDeckSelected(this.props.id);
+  }
+
   render() {
-    return <div className="deck--name">{this.props.name}</div>;
+    return (
+      <div className="deck" onClick={this.onDeckSelected}>
+        <div className="deck--name">{this.props.name}</div>
+      </div>
+    );
   }
 }
