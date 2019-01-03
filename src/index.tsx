b914044b86b541c4593bfc9119a543853ed7d7e7
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-02 19:53:31 -0500

    added: redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { DeckCard } from "./component/DeckCard";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./stores";
+import DeckListContainer from "./containers/DeckListContainer";
 import * as serviceWorker from "./serviceworker/serviceWorker";
+import { initialize } from "./services/api";
 import "./global.css";
 
-ReactDOM.render(
-  <DeckCard front="front" back="back" />,
-  document.getElementById("root")
-);
+(async () => {
+  await initialize();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render(
+    <Provider store={store}>
+      <DeckListContainer />
+    </Provider>,
+    document.getElementById("root")
+  );
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: http://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})();
---
+++ src/reducers/index.ts
@@ -0,0 +1,13 @@
+const defaultState = {
+  decks: [],
+  card: []
+};
+
+const decks = (state = defaultState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default decks;
