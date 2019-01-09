26dbabc48eca5edfc1f88c69396c4c6976f35e28
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-09 16:39:08 -0500

    in progress: facades.spec.ts

---
+++ src/redux/facades.spec.ts
@@ -0,0 +1,20 @@
+import { initializeApi } from "./facades";
+import * as api from "../services/api";
+import * as store from "./store";
+jest.mock("./store");
+jest.mock("../services/api");
+
+afterEach(() => jest.clearAllMocks());
+const dispatchListener = jest.spyOn(store, "dispatch");
+
+describe("initializeApi", () => {
+  it("dispatch is called 2 times", async () => {
+    await initializeApi();
+    expect(dispatchListener.mock.calls.length).toBe(2);
+  });
+
+  it("dispatches error action when getAllDecks is rejected", async () => {
+    jest.spyOn(api, "getAllDecks").mockRejectedValue(null);
+    await initializeApi();
+  });
+});
