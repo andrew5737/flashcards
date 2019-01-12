12b375b16086d8b1d469b52cc81342a4ac3accf4
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-12 08:58:56 -0500

    feat: add CreateDeck Component

---
+++ src/components/CreateDeck/index.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export interface CreateDeckProps {
+  createDeck: (deckName: string) => void;
+  errorMessage: string;
+}
+
+interface CreateDeckState {
+  deckName: string;
+}
+
+export default class CreateDeck extends Component<
+  CreateDeckProps,
+  CreateDeckState
+> {
+  state = { deckName: "" };
+
+  createDeck = () => this.props.createDeck(this.state.deckName);
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    this.setState({ deckName: event.target.value });
+
+  render = () => {
+    return (
+      <div>
+        <span>Creat Deck: </span>
+        <input
+          type="text"
+          placeholder="deck name"
+          onChange={this.handleChange}
+          value={this.state.deckName}
+        />
+        <span onClick={this.createDeck}>➕</span>
+        <span className="error">{this.props.errorMessage}</span>
+      </div>
+    );
+  };
+}
