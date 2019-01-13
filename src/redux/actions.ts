ca5bd564d9653fc5b83f76e5f7060dca2319abe0
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-13 11:25:42 -0500

    featL createDeckAction

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -7,3 +7,9 @@ export const initializeApiAction = createAsyncAction(
   "INITIALIZE_API_SUCCESS",
   "INITIALIZE_API_ERROR"
 )<void, { decks: Deck[]; cards: Card[] }, Error>();
+
+export const createDeckAction = createAsyncAction(
+  "CREATE_DECK_PENDING",
+  "CREATE_DECK_SUCCESS",
+  "CREATE_DECK_ERROR"
+)<void, void, Error>();
