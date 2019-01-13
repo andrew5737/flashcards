1a11303bf0a6f7959010df9d2813b3eabe728492
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-13 11:26:05 -0500

    feat: asyncFacadeBuilder - need to make arguments type safe

--- src/redux/facades.ts
+++ src/redux/facades.ts
@@ -1,15 +1,26 @@
 import { initialize, getAllDecks, getAllCards } from "../services/api";
 import { dispatch } from "./store";
-import { initializeApiAction } from "./actions";
+import { initializeApiAction, createDeckAction } from "./actions";
 
-export const initializeApi = async () => {
-  dispatch(initializeApiAction.request());
+const asyncFacadeBuilder = async (asyncAction: any, asyncFunc: any) => {
+  dispatch(asyncAction.request());
   try {
-    await initialize();
-    const decks = await getAllDecks();
-    const cards = await getAllCards();
-    dispatch(initializeApiAction.success({ decks, cards }));
+    const resp = await asyncFunc();
+    dispatch(asyncAction.success(resp));
   } catch (e) {
-    dispatch(initializeApiAction.failure(e));
+    dispatch(asyncAction.failure(e));
   }
 };
+
+export const initializeApi = async () =>
+  asyncFacadeBuilder(initializeApiAction, async () => {
+    await initialize();
+    const [decks, cards] = await Promise.all([
+      await getAllDecks(),
+      await getAllCards()
+    ]);
+    return { decks, cards };
+  });
+
+export const createDeck = async (name: string) =>
+  asyncFacadeBuilder(createDeckAction, () => createDeck(name));
