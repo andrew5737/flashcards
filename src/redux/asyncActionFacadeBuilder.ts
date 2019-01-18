f372bbfa96115580ede025e4849ad2874ddbffc8
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-18 11:52:26 -0500

    feat: asyncActionFacadeBuilder now accepts cb with arguments

--- src/redux/asyncActionFacadeBuilder.ts
+++ src/redux/asyncActionFacadeBuilder.ts
@@ -12,18 +12,20 @@ export async function asyncActionFacadeBuilder<
   R extends ExtractedRequest<A>,
   S extends ExtractedSuccess<A>,
   F extends ExtractedFailture<A>,
-  P extends ExtractedPayload<S>
+  P extends ExtractedPayload<S>,
+  AF_ARGS extends any[]
 >(
   action: {
     request: R;
     success: S;
     failure: F;
   },
-  asyncFunc: () => Promise<P>
+  asyncFunc: (...args: AF_ARGS) => Promise<P>,
+  ...asyncFuncArgs: AF_ARGS
 ): Promise<void> {
   dispatch(action.request());
   try {
-    const resp = await asyncFunc();
+    const resp = await asyncFunc(...asyncFuncArgs);
     dispatch((action.success as any)(resp));
   } catch (e) {
     dispatch(action.failure(e));
