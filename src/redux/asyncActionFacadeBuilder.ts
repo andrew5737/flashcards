5fefb3da2d561ef62ab8f146c46f32070350ba93
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-18 14:37:52 -0500

    refact: cleaned up asyncActionFacadeBuilder

--- src/redux/asyncActionFacadeBuilder.ts
+++ src/redux/asyncActionFacadeBuilder.ts
@@ -1,34 +1,16 @@
 import { createAsyncAction } from "typesafe-actions";
 import { AsyncActionBuilder } from "typesafe-actions/dist/create-async-action";
 import { dispatch } from "./store";
-import * as actions from "./actions";
 
-type ActionType = typeof actions[keyof typeof actions];
-type ExtractedRequest<A> = A extends { request: infer X } ? X : () => void;
-type ExtractedSuccess<A> = A extends { success: infer X } ? X : () => void;
-type ExtractedFailture<A> = A extends { failure: infer X } ? X : () => void;
-type ExtractedPayload<F> = F extends (args: any) => { payload: infer X } ? X : void;
-
-export async function asyncActionFacadeBuilder<
-  A extends ActionType,
-  R extends ExtractedRequest<A>,
-  S extends ExtractedSuccess<A>,
-  F extends ExtractedFailture<A>,
-  P extends ExtractedPayload<S>,
-  AF_ARGS extends any[]
->(
-  action: {
-    request: R;
-    success: S;
-    failure: F;
-  },
-  asyncFunc: (...args: AF_ARGS) => Promise<P>,
-  ...asyncFuncArgs: AF_ARGS
+async function asyncActionFacadeBuilder<P, A extends any[]>(
+  action: any,
+  asyncFunc: (...args: A) => Promise<P>,
+  ...asyncFuncArgs: A
 ): Promise<void> {
   dispatch(action.request());
   try {
     const resp = await asyncFunc(...asyncFuncArgs);
-    dispatch((action.success as any)(resp));
+    dispatch(action.success(resp));
   } catch (e) {
     dispatch(action.failure(e));
   }
@@ -61,7 +43,7 @@ export function asyncActionCreator<
   const asyncAction = createAsyncAction(requestType, successType, failureType)<void, P, Error>();
 
   const callableAsyncAction: any = (...args: A) =>
-    asyncActionFacadeBuilder(asyncAction as any, cb as any, ...(args as any));
+    asyncActionFacadeBuilder(asyncAction, cb, ...args);
   callableAsyncAction.request = asyncAction.request;
   callableAsyncAction.success = asyncAction.success;
   callableAsyncAction.failure = asyncAction.failure;
