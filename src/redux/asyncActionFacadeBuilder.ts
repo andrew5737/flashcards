e92ded6092fb403100f5cf084bed3acdebeee716
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-18 14:26:41 -0500

    temp: added CallableAsyncAction to facade builder

--- src/redux/asyncActionFacadeBuilder.ts
+++ src/redux/asyncActionFacadeBuilder.ts
@@ -1,3 +1,5 @@
+import { createAsyncAction } from "typesafe-actions";
+import { AsyncActionBuilder } from "typesafe-actions/dist/create-async-action";
 import { dispatch } from "./store";
 import * as actions from "./actions";
 
@@ -30,4 +32,39 @@ export async function asyncActionFacadeBuilder<
   } catch (e) {
     dispatch(action.failure(e));
   }
+}
+
+export interface CallableAsyncAction<
+  T1 extends string,
+  T2 extends string,
+  T3 extends string,
+  P,
+  A extends any[]
+> extends AsyncActionBuilder<T1, T2, T3, void, P, Error> {
+  (...args: A): Promise<void>;
+}
+{
+}
+
+export function asyncActionCreator<
+  T1 extends string,
+  T2 extends string,
+  T3 extends string,
+  A extends any[],
+  P
+>(
+  requestType: T1,
+  successType: T2,
+  failureType: T3,
+  cb: (...args: A) => Promise<P>
+): CallableAsyncAction<T1, T2, T3, P, A> {
+  const asyncAction = createAsyncAction(requestType, successType, failureType)<void, P, Error>();
+
+  const callableAsyncAction: any = (...args: A) =>
+    asyncActionFacadeBuilder(asyncAction as any, cb as any, ...(args as any));
+  callableAsyncAction.request = asyncAction.request;
+  callableAsyncAction.success = asyncAction.success;
+  callableAsyncAction.failure = asyncAction.failure;
+
+  return callableAsyncAction;
 }
