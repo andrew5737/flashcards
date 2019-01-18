cc6f382aad0a08fd7f853d29362c6884d6662c0e
Andrew Smith <andrew.smith5569@gmail.com>
2019-01-18 10:28:51 -0500

    test: completed test for asyncFacadeBuilder

--- src/redux/asyncFacadeBuilder.spec.ts
+++ src/redux/asyncFacadeBuilder.spec.ts
@@ -4,28 +4,75 @@ jest.mock("./store");
 
 const dispatchListener = jest.spyOn(store, "dispatch");
 
+const mockRequestReturnValue = "mockRequestReturnValue";
+const mockSuccessReturnValue = "mockSuccessReturnValue";
+const mockFailureReturnValue = "mockFailureReturnValue";
 const mockAction: { request: any; success: any; failure: any } = {
-  request: jest.fn().mockReturnValue("request"),
-  success: jest.fn().mockReturnValue("success"),
-  failure: jest.fn().mockReturnValue("failure")
+  request: () => mockRequestReturnValue,
+  success: () => mockSuccessReturnValue,
+  failure: () => mockFailureReturnValue
 };
 
 afterEach(() => jest.resetAllMocks());
 
 describe("asyncFacadeBuilder", () => {
-  it("dispatch is called 2 times", async () => {
+  it("calls dispatch twice", async () => {
     const mockAsyncFunc = jest.fn().mockResolvedValue({} as any);
     await asyncFacadeBuilder(mockAction, mockAsyncFunc);
 
     expect(dispatchListener.mock.calls.length).toBe(2);
   });
 
-  it("dispatch recieves request then success", async () => {
+  it("dispatches request then success", async () => {
     const mockAsyncFunc = jest.fn().mockResolvedValue({} as any);
     await asyncFacadeBuilder(mockAction, mockAsyncFunc);
 
     expect(dispatchListener.mock.calls.length).toBe(2);
-    console.log(dispatchListener.mock.calls);
-    expect(dispatchListener.mock.calls[0][0]).toBe("request");
+    expect(dispatchListener.mock.calls[0][0]).toBe(mockRequestReturnValue);
+    expect(dispatchListener.mock.calls[1][0]).toBe(mockSuccessReturnValue);
+  });
+
+  it("dispatches success with the content return by asyncFunc", async () => {
+    const content = { content: "content" };
+    const mockAsyncFunc = () => Promise.resolve(content);
+    const customMockAction: { request: any; success: any; failure: any } = {
+      ...mockAction,
+      success: jest.fn().mockReturnValue(mockSuccessReturnValue)
+    };
+
+    await asyncFacadeBuilder(customMockAction, mockAsyncFunc);
+
+    expect(dispatchListener.mock.calls[1][0]).toBe(mockSuccessReturnValue);
+    expect(customMockAction.success.mock.calls.length).toBe(1);
+    expect(customMockAction.success.mock.calls[0][0]).toBe(content);
+  });
+
+  it("dispatches request then failure when asyncFunction throws an error", async () => {
+    const mockAsyncFunc = () => {
+      throw new Error("Test error");
+    };
+
+    await asyncFacadeBuilder(mockAction, mockAsyncFunc);
+
+    expect(dispatchListener.mock.calls.length).toBe(2);
+    expect(dispatchListener.mock.calls[0][0]).toBe(mockRequestReturnValue);
+    expect(dispatchListener.mock.calls[1][0]).toBe(mockFailureReturnValue);
+  });
+
+  it("on failure dispatches failure with error", async () => {
+    const error = new Error("Test error");
+    const mockAsyncFunc = () => {
+      throw error;
+    };
+    const customMockAction: { request: any; success: any; failure: any } = {
+      ...mockAction,
+      failure: jest.fn().mockReturnValue(mockFailureReturnValue)
+    };
+
+    await asyncFacadeBuilder(customMockAction, mockAsyncFunc);
+
+    expect(dispatchListener.mock.calls[1][0]).toBe(mockFailureReturnValue);
+    expect(customMockAction.failure.mock.calls.length).toBe(1);
+    expect(customMockAction.failure.mock.calls[0][0]).toBe(error);
   });
 });
